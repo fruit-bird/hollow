b4cffb0e72df2617e5ef0744b2d2e69e18624ad3
fruit-bird <github.ru3w9@slmail.me>
2023-02-22 16:54:38 +0100

    turned into lib + small changes

--- output.md
+++ output.md
@@ -1,92 +1,168 @@
-（見る
-と
-Stanley Kubrick 
-hoax 
-地球と
-6.のフットプリント
-アメリカ国旗が建てられました
-). To broadcast the 
-Atlas 
-Apollo Lunarモジュール
+Sputnik 
+ベアスキン
+12人の兄弟
+Moon landings 
+  
+（ロンドンとグラスゴー
+ロシア
+BBC（2016-01-20）。
+A Man on the 
+ distortion, and  
+ジュリー・エンゲルブレヒト
+Shrek Forever After 
+ at  
+Apollo TV camera 
+t 
+, was taken to 
+t
+（また
+Rumpelstiltskin
+悪い女王
+英語に翻訳されています
+「
+月の着陸
+同様の陰謀のアイデア
+ (from  
+Rumpelstiltskin
+「Rumpelstiltskin：魔法
+   
+BBC (2016-01-20).  
+Evil Queen 
+永遠にシュレック
+、
+イングランド
+歪み、そして
+Argonne National Laboratory 
+81 
+アルゴンヌ国立研究所
+ドイツのおとぎ話
+t
+翻訳
+アポロテレビカメラ
+ astronauts using the 
+Eredeti népmesék 
+. 
+。
 アポロを主張しています
-6.のフットプリント
+ussuriisk
+Urdu 
+陰謀理論：私たちはしました
+表示するために
+81
+Cold War 
+jstor
+上の男
+Conspiracy Theory: Did We 
+Hans My Hedgehog 
+: 
+7 
+を使用して宇宙飛行士
+" 
+. 
+Rumpelstinski
+GöttingenSeven
+Rumpelstiltskin 
+Viaf
+Vietnam War 
+冷戦
+「
+) is a German 
+ says in his 
+The Twelve Brothers 
+" 
+Vietnam War 
+Lunar Module descent stages 
+,  
+ヘイズ
+ engines used on 
 ）。
-アポロ11
- With the number 
-）。
-Claims that the Apollo 
- were  
- appears to have 
-6. Footprints in the 
-Apollo Lunar Module 
-MythBusters
-+
+ (1978), which shows 
+ハンス私のハリネズミ
+Translations 
+ (2010) 
+" 
+Moon 
+translated into English 
+）ドイツ語です
+（「ラトルゴースト」）または
+ جعيدان ( 
+および監督
+（澄んだガラス
+で使用されるエンジン
+, and specifying 
+An earlier literary variant 
+、に連れて行かれました
+「
+「
+月のモジュール降下段階
+ ("rattle ghost") or 
+American Imago 
+. 
+Neil Armstrong 
+ロナルドの主張は持っています
+ (a clear glass 
+Rumpelstinski 
+Julie Engelbrecht 
+ウルドゥー語
+Rumpelstiltskin 
+NOVA University Lisbon 
+Russian 
+Sputnik
+German fairy tale 
+t 
+Göttingen Seven 
+ (or  
+ノバ大学リスボン
+ (London and Glasgow 
+eredetinépmesék
+England 
+ベトナム戦争
+月
+初期の文学的なバリアント
+Hebrew 
+ Ronald's claims have 
+" 
+ISSN 
+ベトナム戦争
 solar flares 
- Kaysing was hired 
-Missions tracked by independent 
-大変でした。
- ( 
- (See  
-Linda Dégh 
-extravehicular activity 
-胚外活動
-リンダ・デグ
-宇宙年齢
-Apollo Lunarモジュール
-アポロを主張しています
-と
-Earth and  
-アトラス
-ケイズが雇われました
-胚外活動
-telemetry 
-American flags erected on 
+Space Shuttle 
+medium format 
+歌う、湧き出るひばり
+（2010）
+ロングマン発音辞書
+彼に言います
+「
+ج理（
+The Singing, Springing Lark 
 太陽フレア
-Kharkiv Astronomical Observatory 
-太陽フレア
-+ 
-MythBusters
-宇宙年齢
-大変でした。
-space age 
-リンダ・デグ
-（見る
- it was hard. 
-エアロジン50
-アポロ8
- This computer had 
- in August 2008 
- (October 1957,  
-アトラス
-Moon rocks 
-Apollo 11 
-Aerozine 50 
-そうだった
-+
-Dark Side of the 
-MythBusters 
-space rendezvous 
-Mechanical issues 
-アポロ8
-地球と
-番号で
-lunar meteorites 
-Moon landing conspiracy theories 
-アメリカ国旗が建てられました
-ケイズが雇われました
-エアロジン50
-Roger B. Chaffee 
-Apollo 8 
-+
-テレメトリー
- with  
-テレメトリー
-そうだった
-), from  
- ( 
-+ 
-番号で
-機械的な問題
-アポロ11
-機械的な問題
-+
+, a former  
+ similar conspiracy ideas 
+haze 
+"Rumpelstiltskin: The magic of 
+スペースシャトル
+7
+アメリカのイメイゴ
+" 
+中型形式
+Bearskin 
+ニール・アームストロング
+Ussuriisk 
+ヘブライ語
+In order to appear 
+で
+（1978）、それを示しています
+" 
+Claims that the Apollo 
+：
+（から
+ and directed by 
+ in 2005. 
+JSTOR 
+、および指定
+Longman Pronunciation Dictionary 
+「
+2005年。
+。
+、 前者
+。
+ISSN
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,15 @@
-// have this shit take 2 wiki links
-// 1 for the normal topic (say Shrek 2)
-// 2 for the conspiracy topic (say 911 conspiracies)
-// Then port this to a simple wasm website, after making this code organized and shit
-#![allow(unused)]
+//! hell Shallow 走私 毒品 賣淫 春畫 賭博 六合彩 天安門
+//!
+//! hollow shell 疆維吾爾自治
+//!
+//! branded like Cattle安門 天安门 法輪功 李洪志
 
 use anyhow::Result;
 use rand::{seq::SliceRandom, Rng};
 use scraper::{Html, Selector};
-use std::ops::Range;
 
-// #[derive(Debug)]
-// pub enum PromptError {
-//     InvalidURL,
-//     CouldNotFetch,
-// }
-
-// impl std::fmt::Display for PromptError {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         f.write_str(stringify!(self))
-//     }
-// }
-
-// impl std::error::Error for PromptError {}
-
-#[derive(Debug, Default)]
-struct Prompt {
+#[derive(Debug)]
+pub struct Prompt {
     normal_link: String,
     conspiracy_link: String,
     second_language: String,
@@ -32,7 +17,7 @@ struct Prompt {
 
 impl Prompt {
     /// Links are assumed to be valid wiki article links
-    fn new(normal_link: &str, conspiracy_link: &str, language: &str) -> Prompt {
+    pub fn new(normal_link: &str, conspiracy_link: &str, language: &str) -> Prompt {
         Prompt {
             normal_link: normal_link.to_string(),
             conspiracy_link: conspiracy_link.to_string(),
@@ -87,13 +72,13 @@ impl Prompt {
     }
 }
 
-pub fn run(normal: &str, conspiracy: &str, language: &str) -> Result<()> {
-    let prompt = Prompt::new(normal, conspiracy, language);
+pub fn run(prompt: Prompt) -> Result<Vec<String>> {
+    // let prompt = Prompt::new(normal, conspiracy, language);
     let normal_entries = prompt.get_normal_entries()?;
     let conspiracy_entries = prompt.get_conspiracy_entries()?;
 
-    let normal_translation = translator(&normal_entries, language);
-    let conspiracy_translation = translator(&conspiracy_entries, language);
+    let normal_translation = translator(&normal_entries, &prompt.second_language);
+    let conspiracy_translation = translator(&conspiracy_entries, &prompt.second_language);
 
     let normal_mix =
         normal_entries
@@ -118,9 +103,7 @@ pub fn run(normal: &str, conspiracy: &str, language: &str) -> Result<()> {
     entries.extend(conspiracy_mix);
     entries.shuffle(&mut rand::thread_rng());
 
-    println!("{}", entries.join("\n"));
-
-    Ok(())
+    Ok(entries)
 }
 
 /// Returns a vector with the translations, excluding empty lines
@@ -133,11 +116,11 @@ async fn translator(text_vec: &[String], language: &str) -> Vec<String> {
     let translation = google_translator::translate(text_vec.to_vec(), "auto", language)
         .await
         .unwrap();
-    dbg!(&translation.output_text);
+
     let v = translation
         .output_text
         .iter()
-        .filter_map(|v| v.first()) // first translation, remove alternatives
+        .filter_map(|v| v.first()) // take first translation, remove alternatives
         .filter(|&s| !s.contains("\n"))
         .map(|s| s.to_owned())
         .collect();
