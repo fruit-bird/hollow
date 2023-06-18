9923c7a7d54739d4094145e091f8b02465de362a
fruit-bird <github.ru3w9@slmail.me>
2023-06-18 23:28:45 +0100

    moved CLI elsewhere + using wikipedia api + restructuring

--- src/lib.rs
+++ src/lib.rs
@@ -1,142 +1,119 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use rand::{seq::SliceRandom, Rng};
-use scraper::{Html, Selector};
 
 const WIKI_URL: &str = "https://en.wikipedia.org/wiki/";
+const WIKI_API_URL: &str = "https://en.wikipedia.org/w/api.php";
 
-#[derive(Debug)]
 pub struct Hollow<'a> {
-    first_link: &'a str,
-    second_link: &'a str,
+    first: &'a str,
+    second: &'a str,
     second_language: &'a str,
 }
 
 impl<'a> Hollow<'a> {
-    pub fn new(first_link: &'a str, second_link: &'a str, second_language: &'a str) -> Self {
+    pub fn new(first: &'a str, second: &'a str, second_language: &'a str) -> Self {
         Self {
-            first_link,
-            second_link,
+            first,
+            second,
             second_language,
         }
     }
 
-    async fn get_normal_entries(&self) -> Result<Vec<String>> {
-        let response = if self.first_link.starts_with(WIKI_URL) {
-            reqwest::get(self.first_link).await?.text().await?
-        } else {
-            reqwest::get(format!(
-                "https://en.wikipedia.org/wiki/{}",
-                &self.first_link
-            ))
-            .await?
-            .text()
-            .await?
-        };
-
-        let document = Html::parse_document(&response);
-        let content_selector = Selector::parse("#mw-content-text").unwrap();
-        let content_element = document.select(&content_selector).next().unwrap();
-
-        let mut rng = rand::thread_rng();
-        let vec_text = content_element
-            .text()
-            .filter(|s| s.len() < 60)
-            .filter_map(|s| {
-                match s.contains("\n") || s.contains("\u{a0}") || s.contains("[") || s.contains("]")
-                {
-                    true => None,
-                    false => Some(s.split(' ').take(4).map(|s| s.to_string() + " ").collect()),
-                }
-            })
-            .step_by(rng.gen_range(5..21))
-            .take(rng.gen_range(30..60))
-            .collect();
-
-        Ok(vec_text)
-    }
-
-    async fn get_conspiracy_entries(&self) -> Result<Vec<String>> {
-        let response = if self.second_link.starts_with(WIKI_URL) {
-            reqwest::get(self.second_link).await?.text().await?
-        } else {
-            // in case the input is a topic rather than a link
-            reqwest::get(format!(
-                "https://en.wikipedia.org/wiki/{}",
-                &self.second_link
-            ))
-            .await?
-            .text()
-            .await?
-        };
-
-        let document = Html::parse_document(&response);
-        let content_selector = Selector::parse("#mw-content-text").unwrap();
-        let content_element = document.select(&content_selector).next().unwrap();
-
-        let mut rng = rand::thread_rng();
-        let vec_text = content_element
-            .text()
-            .filter(|s| s.len() < 60)
-            .filter_map(|s| {
-                match s.contains("\n") || s.contains("\u{a0}") || s.contains("[") || s.contains("]")
-                {
-                    true => None,
-                    false => Some(s.split(' ').take(4).map(|s| s.to_string() + " ").collect()),
-                }
-            })
-            .step_by(rng.gen_range(5..21))
-            .take(rng.gen_range(30..60))
-            .collect();
-
-        Ok(vec_text)
-    }
-
-    async fn translator(
-        &self,
-        normal_entries: &[String],
-        conspiracy_entries: &[String],
-    ) -> Result<(Vec<String>, Vec<String>)> {
-        let normal_translation =
-            google_translator::translate(normal_entries, "auto", self.second_language)
-                .await?
-                .output_text
-                .iter()
-                .filter_map(|v| v.first()) // take first translation, remove alternatives
-                .filter(|s| !s.contains("\n"))
-                .map(ToString::to_string)
-                .collect();
-
-        let conspiracy_translation =
-            google_translator::translate(conspiracy_entries, "auto", self.second_language)
-                .await?
-                .output_text
-                .iter()
-                .filter_map(|v| v.first()) // take first translation, remove alternatives
-                .filter(|s| !s.contains("\n"))
-                .map(ToString::to_string)
-                .collect();
-        Ok((normal_translation, conspiracy_translation))
-    }
-
     pub async fn run(&self) -> Result<String> {
-        let normal_entries = self.get_normal_entries().await?;
-        let conspiracy_entries = self.get_conspiracy_entries().await?;
+        let first_entries = get_entries(self.first).await?;
+        let second_entries = get_entries(self.second).await?;
 
-        let (normal_translation, conspiracy_translation) = self
-            .translator(&normal_entries, &conspiracy_entries)
-            .await?;
+        let first_translation = translator(&first_entries, self.second_language).await?;
+        let second_translation = translator(&first_entries, self.second_language).await?;
 
-        let normal_mix = combine_original_and_translation(normal_entries, normal_translation);
-        let conspiracy_mix =
-            combine_original_and_translation(conspiracy_entries, conspiracy_translation);
+        let first_mix = combine_original_and_translation(first_entries, first_translation);
+        let second_mix = combine_original_and_translation(second_entries, second_translation);
 
         let mut entries = vec![];
-        entries.extend(normal_mix);
-        entries.extend(conspiracy_mix);
+        entries.extend(first_mix);
+        entries.extend(second_mix);
         entries.shuffle(&mut rand::thread_rng());
 
         Ok(entries.join(" "))
     }
+}
+
+// query is not a wiki url, it's a search query
+// TODO give option to pass both this or just a wiki link
+async fn get_wiki_article(query: &str) -> Result<String> {
+    // https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&titles=Artificial%20intelligence
+    let params = serde_json::json! {
+        {
+            "action": "query",
+            "format": "json",
+            "redirects": "resolve",
+            "exlimit": 0,
+            "explaintext": 1,
+            "titles": query,
+            "prop": "extracts",
+            // "action", "opensearch"
+        }
+    };
+
+    if query.starts_with(WIKI_URL) {
+        todo!("Support for wiki links has yet to be implemented")
+    }
+
+    let client = reqwest::Client::new();
+    let response = client.get(WIKI_API_URL).query(&params).send().await?;
+    let response_text = response.text().await?;
+    let parsed_response = serde_json::from_str::<serde_json::Value>(&response_text)?;
+
+    let pages = parsed_response["query"]["pages"]
+        .as_object()
+        .ok_or(anyhow!("No pages returned"))?;
+    let page = pages.iter().next().ok_or(anyhow!("No pages returned"))?.1;
+    let article_text = page["extract"]
+        .as_str()
+        .ok_or(anyhow!("Could not extract content"))?
+        .to_string();
+
+    Ok(article_text)
+}
+
+async fn get_entries(query: &str) -> Result<Vec<String>> {
+    let article_text = get_wiki_article(query).await?;
+
+    let vec_text: Vec<String> = article_text
+        .lines()
+        .filter(|s| s.len() > 5)
+        .filter_map(
+            |s| match s.contains("\n") || s.starts_with("=") || s.starts_with(" ") {
+                true => None,
+                false => Some(
+                    s.split(' ')
+                        .skip(2) // most first words are the/on...
+                        .step_by(rand::thread_rng().gen_range(2..6))
+                        .take(4) // 4 or 5 seems to be the spot
+                        .map(|s| s.to_string() + " ")
+                        .collect(),
+                ),
+            },
+        )
+        .step_by(rand::thread_rng().gen_range(2..4)) // 5..21
+        .take(rand::thread_rng().gen_range(30..60)) // 30..60
+        .collect();
+    dbg!(&vec_text);
+
+    Ok(vec_text)
+}
+
+async fn translator(entries: &[String], second_language: &str) -> Result<Vec<String>> {
+    let translation = google_translator::translate(entries, "auto", second_language)
+        .await?
+        .output_text
+        .iter()
+        .filter_map(|v| v.first()) // take first translation, remove alternatives
+        .filter(|s| !s.contains("\n"))
+        .map(ToString::to_string)
+        .collect();
+
+    Ok(translation)
 }
 
 fn combine_original_and_translation(entries: Vec<String>, translation: Vec<String>) -> Vec<String> {
